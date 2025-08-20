c421ad98173b627d728014d674845b3ea6d2c49f
Marvin Luchutaskul <marvin.luchutaskul@gmail.com>
2025-08-20 18:52:06 +0700

    document title update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useLocation, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react'; 
 import Header from './Header'
 import HunterBuilder from './HunterBuilder';
 import AtkspdCalc from './AtkspdCalc';
@@ -6,14 +7,33 @@ import Footer from './Footer';
 
 
 function App() {
+
+	function PageTitleupdater() {
+		const location = useLocation();
+
+		useEffect(() => {
+			const titles = {
+				"/builder": "Builder",
+				"/atkspd": "ATK SPD",
+				"/index": "Index"
+			};
+
+			document.title = titles[location.pathname] || "Home";
+		}, [location.pathname])
+
+		return null;
+	}
   
 	return(
 		<Router>
 			<Header></Header>
 
+			<PageTitleupdater></PageTitleupdater>
+
 			<Routes>
 				<Route path='/builder' element={<HunterBuilder />} />
 				<Route path='/atkspd' element={<AtkspdCalc />} />
+				
 			</Routes>
 
 			<Footer></Footer>
