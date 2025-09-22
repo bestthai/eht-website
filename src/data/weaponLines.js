8868d33fc587dcd680b9c2038fb7dc44ed587f45
bestthai <marvin.luchutaskul@gmail.com>
2025-09-22 15:49:21 +0700

    fix gear and weapon value mismatch

--- src/data/weaponLines.js
+++ src/data/weaponLines.js
@@ -146,94 +146,66 @@ export const weaponLineGroups = {
 
 };
 
-export const ancientWeaponValues = {}
-    weaponLines.slice(0, 6).forEach(line => {
-        ancientWeaponValues[line] = [3, 10];
-    });
+// ---------------- VALUE GROUPS ----------------
+// Match old difficulty ranges to new order
+const Group1 = [
+    "Attack Speed",
+    "Critical Hit Chance",
+    "Evasion",
+    "EXP Gain",
+    "Chance to transform into Archangel",
+    "Chance to transform into Demon Lord",
+];
+const Group2 = [
+    "Decrease Damage Taken",
+    "Decrease Mood Consumption",
+    "Decrease Satiety Consumption",
+    "Decrease Stamina Consumption",
+    "Lifesteal",
+    "Increase Movement Speed",
+    "Chance to gain 2x Gold",
+    "Chance to gain extra Material",
+    "Chance to cast level 1 Evasion",
+    "Chance to cast level 1 Barrier",
+    "Chance to cast level 1 Curse",
+    "Chance to recover 1% Stamina",
+    "Chance to recover 3% Satiety",
+    "Chance to recover 3% Mood",
+    "Chance to stun enemy for 2 sec",
+];
+const Group3 = ["Attack", "Defense", "HP", "Additional Damage"];
+const Group4 = [
+    "Critical Hit Damage",
+    "Increase Damage Against Boss",
+    "Increase Damage Against Primate",
+    "Increase Damage Against Demon",
+    "Increase Damage Against Animal",
+    "Increase Damage Against Undead",
+];
 
-    weaponLines.slice(6, 21).forEach(line => {
-        ancientWeaponValues[line] = [5, 15];
-    });
+// ---------------- ASSIGN HELPER ----------------
+function assignWeaponValues(target, values) {
+    Group1.forEach(line => (target[line] = values[0]));
+    Group2.forEach(line => (target[line] = values[1]));
+    Group3.forEach(line => (target[line] = values[2]));
+    Group4.forEach(line => (target[line] = values[3]));
+}
 
-    weaponLines.slice(21, 25).forEach(line => {
-        ancientWeaponValues[line] = [8, 20];
-    });
-
-    weaponLines.slice(25, 31).forEach(line => {
-        ancientWeaponValues[line] = [15, 30];
-    });    
-
+// ---------------- WEAPON VALUES ----------------
+export const ancientWeaponValues = {};
+assignWeaponValues(ancientWeaponValues, [[3, 10], [5, 15], [8, 20], [15, 30]]);
 
 export const primalWeaponValues = {};
-    weaponLines.slice(0, 6).forEach(line => {
-        primalWeaponValues[line] = [5, 12];
-    });
-
-    weaponLines.slice(6, 21).forEach(line => {
-        primalWeaponValues[line] = [7, 17];
-    });
-
-    weaponLines.slice(21, 25).forEach(line => {
-        primalWeaponValues[line] = [11, 23];
-    });
-
-    weaponLines.slice(25, 31).forEach(line => {
-        primalWeaponValues[line] = [20, 35];
-    });
-
+assignWeaponValues(primalWeaponValues, [[5, 12], [7, 17], [11, 23], [20, 35]]);
 
 export const originalWeaponValues = {};
-    weaponLines.slice(0, 6).forEach(line => {
-        originalWeaponValues[line] = [5, 12];
-    });
-
-    weaponLines.slice(6, 21).forEach(line => {
-        originalWeaponValues[line] = [7, 17];
-    });
-
-    weaponLines.slice(21, 25).forEach(line => {
-        originalWeaponValues[line] = [11, 23];
-    });
-
-    weaponLines.slice(25, 31).forEach(line => {
-        originalWeaponValues[line] = [20, 35];
-    });
-
+assignWeaponValues(originalWeaponValues, [[5, 12], [7, 17], [11, 23], [20, 35]]);
 
 export const chaosWeaponValues = {};
-    weaponLines.slice(0, 6).forEach(line => {
-        chaosWeaponValues[line] = [7, 14];
-    });
-
-    weaponLines.slice(6, 21).forEach(line => {
-        chaosWeaponValues[line] = [9, 19];
-    });
-
-    weaponLines.slice(21, 25).forEach(line => {
-        chaosWeaponValues[line] = [14, 26];
-    });
-
-    weaponLines.slice(25, 31).forEach(line => {
-        chaosWeaponValues[line] = [25, 40];
-    });
-
+assignWeaponValues(chaosWeaponValues, [[7, 14], [9, 19], [14, 26], [25, 40]]);
 
 export const abyssWeaponValues = {};
-    weaponLines.slice(0, 6).forEach(line => {
-        abyssWeaponValues[line] = [9, 16];
-    });
-
-    weaponLines.slice(6, 21).forEach(line => {
-        abyssWeaponValues[line] = [11, 21];
-    });
-    
-    weaponLines.slice(21, 25).forEach(line => {
-        abyssWeaponValues[line] = [17, 29];
-    });
-
-    weaponLines.slice(25, 31).forEach(line => {
-        abyssWeaponValues[line] = [30, 45];
-    });
+assignWeaponValues(abyssWeaponValues, [[9, 16], [11, 21], [17, 29], [30, 45]]);
 
 export const weaponValuesByType = {
   ancient: ancientWeaponValues,
